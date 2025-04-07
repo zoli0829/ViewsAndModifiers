15b1048dad2a49e0f4dc3fd6a3e9fce0a41084bf
zoli0829 <zoli0829@gmail.com>
2025-04-07 17:57:38 +0100

    Initial commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Zoltan Vegh on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
