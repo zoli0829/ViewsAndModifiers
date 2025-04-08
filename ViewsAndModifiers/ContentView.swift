0c83c97de3646cae6450a49a4808a2d08862dcc8
zoli0829 <zoli0829@gmail.com>
2025-04-08 18:27:02 +0100

    finished day 23

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,11 +7,28 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Text("Hello World")
-        }
+        Text("Hello World!")
+            .titleStyle()
     }
 }
 
